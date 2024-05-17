9ce1a9f986a8576996becdb57c2dc7cbe88ce7e3
Abhilash Bag <102401165+abhilashbag@users.noreply.github.com>
2024-05-17 19:49:04 +0530

    alert

--- auth.js
+++ auth.js
@@ -33,7 +33,9 @@ function logout() {
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
         console.log("User is signed in.");
+        alert("user signed in")
     } else {
         console.log("No user is signed in.");
+        alert("user signed in")
     }
 });
